5f22bf508b387a55ddd532ddd189668a960417e9
tannistha12 <tannisthachat17@gmail.com>
2025-08-09 18:57:17 +0530

    count words

---
+++ main2.java
@@ -0,0 +1,7 @@
+class main2{
+    public static void main(String[] args) {
+        String words = "One Two Three Four Five Six Seven Eight Nine Ten";
+        int count = words.split("\\s+").length;
+        System.out.println("The number of words in the sentence is: " + count);
+    }
+}
